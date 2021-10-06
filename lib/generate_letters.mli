4a6e4a4e0259705eef83ed85ae7b163529c2e8c0
Kyra Lee <kl627@cornell.edu>
2021-10-06 12:48:44 -0400

    updates mli

--- lib/generate_letters.mli
+++ lib/generate_letters.mli
@@ -7,3 +7,5 @@ val start_game : char list -> char list
 val remove_let : int -> int -> letter_deck -> letter_deck
 
 val index_of_letter : int -> char -> letter_deck -> int
+
+val optimize_start : unit -> char list
