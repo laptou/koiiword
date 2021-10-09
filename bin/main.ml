7e58acaac7420f41b1a73435a6d7bfd7ea791337
Ibiyemi Abiodun <ibiyemi@intulon.com>
2021-10-09 01:47:12 -0400

    add all of the layout boxes

--- bin/main.ml
+++ bin/main.ml
@@ -12,6 +12,7 @@ open LTerm_geom
 open LTerm_key
 open Koiiword.Board
 open Koiiword.State
+open Koiiword.Layout
 open CamomileLibrary
 
 (** The [loop_result] type describes the response of the gameplay loop
@@ -61,7 +62,7 @@ let rec loop (ui : LTerm_ui.t) (game_state : game_state ref) :
       loop ui game_state
   | LoopResultContinue -> loop ui game_state
 
-let draw_grid ctx rect =
+let draw_board_gridlines ctx rect =
   let rec range start stop step fn =
     fn start;
     if start + step < stop then range (start + step) stop step fn
@@ -75,13 +76,38 @@ let draw_grid ctx rect =
 
   (* draw hlines *)
   range rect.row1 rect.row2 spacing (fun row ->
-      LTerm_draw.draw_hline ctx (rect.row1 + row) rect.col1 width ~style
+      LTerm_draw.draw_hline ctx row rect.col1 width ~style
         LTerm_draw.Light);
 
   (* draw vlines *)
   range rect.col1 rect.col2 spacing (fun col ->
-      LTerm_draw.draw_vline ctx rect.row1 (rect.col1 + col) height
-        ~style LTerm_draw.Light)
+      LTerm_draw.draw_vline ctx rect.row1 col height ~style
+        LTerm_draw.Light)
+
+let with_grid_cell
+    ctx
+    layout_spec
+    row_start
+    row_span
+    col_start
+    col_span
+    fn =
+  let ctx_size = LTerm_draw.size ctx in
+  let ctx_rect =
+    { row1 = 0; row2 = ctx_size.rows; col1 = 0; col2 = ctx_size.cols }
+  in
+  let abs_bounds =
+    get_grid_rect ctx_rect layout_spec row_start row_span col_start
+      col_span
+  in
+  let ctx = LTerm_draw.sub ctx abs_bounds in
+  let size = LTerm_draw.size ctx in
+  let rel_bounds =
+    { row1 = 0; col1 = 0; row2 = size.rows; col2 = size.cols }
+  in
+  fn ctx rel_bounds
+
+let layout_spec = { cols = [ 0.3; 0.7 ]; rows = [ 0.4; 0.6 ] }
 
 (** The renderer. This takes game state and a terminal UI object and
     renders the game according to its current state. *)
@@ -91,29 +117,47 @@ let draw ui_terminal matrix (game_state : game_state) =
   LTerm_draw.clear ctx;
   LTerm_draw.draw_hline ctx 0 0 size.cols LTerm_draw.Heavy;
   LTerm_draw.draw_string_aligned ctx 0 H_align_center
+    ~style:{ LTerm_style.none with bold = Some true }
     (Zed_string.of_utf8 " koiiword ");
-  LTerm_draw.draw_frame ctx
-    { row1 = 1; col1 = 1; row2 = size.rows - 1; col2 = size.cols - 1 }
-    LTerm_draw.Heavy;
-  if size.rows > 2 && size.cols > 2 then (
-    let ctx =
-      LTerm_draw.sub ctx
-        {
-          row1 = 1;
-          col1 = 1;
-          row2 = size.rows - 1;
-          col2 = size.cols - 1;
-        }
+  if size.rows < 15 || size.cols < 30 then
+    LTerm_draw.draw_string_aligned ctx (size.rows / 2) H_align_center
+      (Zed_string.of_utf8 "please make your terminal bigger")
+  else
+    let rect =
+      { row1 = 1; col1 = 0; row2 = size.rows - 1; col2 = size.cols }
     in
-    let size = LTerm_draw.size ctx in
-    draw_grid ctx
-      { row1 = 1; col1 = 1; row2 = size.rows - 1; col2 = size.cols - 1 };
-    let row, col = game_state.board.cursor in
-    let style = { LTerm_style.none with reverse = Some true } in
-    LTerm_draw.draw_char ctx
-      ((row * 2) + 1)
-      ((col * 2) + 1)
-      ~style (Zed_char.of_utf8 "*"))
+    let ctx = LTerm_draw.sub ctx rect in
+    (* draw board *)
+    with_grid_cell ctx layout_spec 0 2 1 2 (fun ctx bounds ->
+        LTerm_draw.draw_frame_labelled ctx bounds
+          (Zed_string.of_utf8 "board")
+          LTerm_draw.Heavy;
+        draw_board_gridlines ctx (inset bounds 1);
+        let cursor_ctx = LTerm_draw.sub ctx (inset bounds 1) in
+        let row, col = game_state.board.cursor in
+        LTerm_draw.draw_char cursor_ctx
+          ((row * 2) + 1)
+          ((col * 2) + 1)
+          ~style:{ LTerm_style.none with reverse = Some true }
+          (Zed_char.of_utf8 "*"));
+    (* draw players box *)
+    with_grid_cell ctx layout_spec 0 1 0 1 (fun ctx bounds ->
+        LTerm_draw.draw_frame_labelled ctx bounds
+          (Zed_string.of_utf8 "players")
+          LTerm_draw.Heavy);
+    (* draw letters box *)
+    with_grid_cell ctx layout_spec 1 2 0 1 (fun ctx bounds ->
+        LTerm_draw.draw_frame_labelled ctx bounds
+          (Zed_string.of_utf8 "letters")
+          LTerm_draw.Heavy);
+    (* draw prompt box *)
+    with_grid_cell ctx layout_spec 2 1 1 1 (fun ctx bounds ->
+        LTerm_draw.draw_frame ctx bounds LTerm_draw.Heavy);
+    (* draw selection box *)
+    with_grid_cell ctx layout_spec 2 1 2 1 (fun ctx bounds ->
+        LTerm_draw.draw_frame_labelled ctx bounds
+          (Zed_string.of_utf8 "selection")
+          LTerm_draw.Heavy)
 
 let main () =
   let%lwt term = Lazy.force LTerm.stdout in
