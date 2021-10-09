38cfe9950ba9a6bd9fba8a71f0606381b6876a35
Ibiyemi Abiodun <ibiyemi@intulon.com>
2021-10-09 02:05:27 -0400

    delete license statement at the top of main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,12 +1,3 @@
-(*
- * move.ml
- * -------
- * Copyright : (c) 2011, Jeremie Dimino <jeremie@dimino.org>
- * Licence   : BSD3
- *
- * This file is a part of Lambda-Term.
- *)
-
 open Lwt
 open LTerm_geom
 
