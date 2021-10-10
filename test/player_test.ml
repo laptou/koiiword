030267367e27a6627703cf34c507e79a153a2863
Owen Ralbovsky <oxr2@cornell.edu>
2021-10-09 21:35:01 -0400

    Rephrase cplayer test names

--- test/player_test.ml
+++ test/player_test.ml
@@ -24,10 +24,10 @@ let test_cases =
       "[player1; player2; player3] player 1 next is player 2" player1
       player_lst player2;
     next_player_test
-      "[player1; player2; player3] player 1 next is player 2" player2
+      "[player1; player2; player3] player 2 next is player 3" player2
       player_lst player3;
     next_player_test
-      "[player1; player2; player3] player 1 next is player 2" player3
+      "[player1; player2; player3] player 3 next is player 1" player3
       player_lst player1;
   ]
 
