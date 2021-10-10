dc672bd62aa4c80b58e600e839833aca19f83cf1
Owen Ralbovsky <oxr2@cornell.edu>
2021-10-10 16:36:49 -0400

    Add specification for type player in player.ml

--- lib/player.ml
+++ lib/player.ml
@@ -1,5 +1,8 @@
-(* Type for each player *)
 type player = { letters : Generate_letters.letter_deck }
+(** type [player] defines the player.
+
+    - Has field [letters] used to store the letters they have in their
+      hand. *)
 
 (** [advance_players player_lst] gives the same player list except the
     head player is moved to the last index. Returns empty list if empty
