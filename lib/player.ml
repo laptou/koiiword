4ca466bd62ea8437186776be1a9967f7946c869f
Owen Ralbovsky <oxr2@cornell.edu>
2021-10-13 18:03:12 -0400

    Refactor and make changes (commented below) - Remove advance_players method in player.ml (unused) - Remove player_test.ml as it has no methods - Add state_test.ml and test cases for next_player method

--- lib/player.ml
+++ lib/player.ml
@@ -7,9 +7,3 @@ type player = {
 
     - Has field [letters] used to store the letters they have in their
       hand. *)
-
-(** [advance_players player_lst] gives the same player list except the
-    head player is moved to the last index. Returns empty list if empty
-    list is given. Ex: [\[p1; p2; p3\]] -> [\[p2; p3; p1\]]*)
-let advance_players (player_lst : player list) =
-  match player_lst with [] -> [] | h :: t -> t @ [ h ]
--- test/state_test.ml
+++ test/state_test.ml
@@ -1,13 +1,15 @@
 open OUnit2
 open Koiiword.Player
 open Koiiword.Generate_letters
+open Koiiword.State
 
-let advance_players_test
+let test_next_player
     (name : string)
-    (player_lst : player list)
-    (expected_player_list : player list) : test =
+    (curr_player : player)
+    (players : player list)
+    (expected_output : player) : test =
   name >:: fun _ ->
-  assert_equal (advance_players player_lst) expected_player_list
+  assert_equal (next_player curr_player players) expected_output
 
 let player1 = { name = "P1"; points = 50; letters = start_game () }
 
@@ -19,11 +21,9 @@ let player_lst = [ player1; player2; player3 ]
 
 let test_cases =
   [
-    advance_players_test
-      "[player1; player2; player3] becomes [player2; player3; player1]"
-      [ player1; player2; player3 ]
-      [ player2; player3; player1 ];
-    advance_players_test "[] becomes []" [] [];
+    test_next_player "P1 next player is P2" player1 player_lst player2;
+    test_next_player "P2 next player is P3" player2 player_lst player3;
+    test_next_player "P3 next player is P1" player3 player_lst player1;
   ]
 
 let suite = "test suite for Players" >::: test_cases
