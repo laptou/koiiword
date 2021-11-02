5bb13a5fef3f7288e9e20fc92be0263cb572d712
Owen Ralbovsky <oxr2@cornell.edu>
2021-11-02 04:09:12 -0400

    Update specs in dictionary.ml and dictionary.mli

--- lib/dictionary.ml
+++ lib/dictionary.ml
@@ -1,9 +1,9 @@
-(* type [dictionary] is the type for the HashTable containing the
-   dictionary contents *)
 type dictionary = (string, string) Hashtbl.t
+(** type [dictionary] is the type for the HashTable containing the
+    dictionary contents *)
 
-(* [dict_from_file] takes in [file_name] string to give a dictionary of
-   type [dictionary]*)
+(** [dict_from_file] takes in [file_name] string to give a dictionary of
+    type [dictionary]*)
 let dict_from_file (file_name : string) : dictionary =
   let ic = open_in file_name in
   let ht = Hashtbl.create 400000 in
@@ -17,8 +17,8 @@ let dict_from_file (file_name : string) : dictionary =
     close_in ic;
     ht
 
-(* [is_word_valid] takes in a [dictionary] dictionary and a [word]
-   string and returns true if [word] is in [dictionary] and false
-   otherwise *)
+(** [is_word_valid] takes in a [dictionary] dictionary and a [word]
+    string and returns true if [word] is in [dictionary] and false
+    otherwise *)
 let is_word_valid (dict : dictionary) (word : string) =
   Hashtbl.mem dict (String.uppercase_ascii word)
